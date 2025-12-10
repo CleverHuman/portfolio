d3685f5330e4b5ad156bf91b5feae717aa2978dd
cleverhuman <cleverhuman110@gmail.com>
2025-12-10 01:21:16 -0800

    update title.

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'My Portfolio',
+  title: 'Sigma Seven',
   description: 'Showcasing my work and projects',
 };
 
